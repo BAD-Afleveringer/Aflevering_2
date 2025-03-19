14ab95bb51fd9d2d27706511037415bb6c2b76c1
LarsUnder <Mikkelmkrb@gmail.com>
2025-03-19 14:38:26 +0100

    correct migrations added, when adding later pushes remember to add back in migrations to gitignore :)

---
+++ Migrations/20250319133512_Migration2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aflevering_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migration2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CVR",
+                table: "Providers");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CVR",
+                table: "Providers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Providers",
+                keyColumn: "ProviderId",
+                keyValue: 1,
+                column: "CVR",
+                value: 12345678);
+
+            migrationBuilder.UpdateData(
+                table: "Providers",
+                keyColumn: "ProviderId",
+                keyValue: 2,
+                column: "CVR",
+                value: 87654321);
+
+            migrationBuilder.UpdateData(
+                table: "Providers",
+                keyColumn: "ProviderId",
+                keyValue: 3,
+                column: "CVR",
+                value: 87652321);
+
+            migrationBuilder.UpdateData(
+                table: "Providers",
+                keyColumn: "ProviderId",
+                keyValue: 4,
+                column: "CVR",
+                value: 82654321);
+        }
+    }
+}
